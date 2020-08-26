78a1534d3d967b8654af7c74b920f0e66888a655
sblakely <stephanye.blakely@gmail.com>
2020-08-26 19:31:51 -0400

    API Post request

--- server/index.js
+++ server/index.js
@@ -21,4 +21,22 @@ app.get('/hostels/:id/api/reviews', (req, res) => {
   });
 });
 
+app.post('/hostles/:id/api/reviews', (req, res) => {
+  const total = ((req.body.security + req.body.location + req.body.staff + req.body.atmosphere + req.body.cleanliness + req.body.facilities + req.body.value) / 7).toFixed(1);
+  const queryStr =`INSERT INTO authors (authdescription, name, gender, age_group, picture_url) VALUES ("${req.body.authdescription}", "${req.body.name}", "${req.body.gender}", "${req.body.age_group}", "${req.body.picture_url}");
+  SELECT id INTO @last_author_id FROM authors WHERE authors.name = "${req.body.name}";
+  INSERT INTO reviews (hostel_id, author_id, description, security, location, staff, atmosphere, cleanliness, facilities, value, total, created_at ) VALUES (${req.params.id}, @last_author_id, "${req.body.description}", ${req.body.security}, ${req.body.location}, ${req.body.staff}, ${req.body.atmosphere}, ${req.body.cleanliness}, ${req.body.facilities}, ${req.body.value}, ${total}, "${req.body.created_at}");`;
+  db.connection.query(queryStr, (err, response) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      console.log('done');
+      res.json(response);
+    }
+  });
+});
+
+
+
 app.listen(3001, () => console.log('listening on 3001'));
